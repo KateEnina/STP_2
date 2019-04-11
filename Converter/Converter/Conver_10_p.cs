342b4dfb884c0a52272b57b8662a7b8935f294a0
KateStich <ekate.enina@mail.ru>
2019-04-11 19:54:35 +0700

    add lab converter

---
+++ Converter/Converter/Conver_10_p.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace Converter {
+	public class Conver_10_p {
+		public static string Do(double n, int p, int c) {
+			if (p < 2 || p > 16)
+				throw new IndexOutOfRangeException();
+			if (c < 0 || c > 10)
+				throw new IndexOutOfRangeException();
+
+			long LeftSide = (long)n;
+
+			double RightSide = 0f;
+			RightSide = n - LeftSide;
+			if (RightSide < 0)
+				RightSide *= -1;
+
+			string LeftSideString = int_to_P(LeftSide, p);
+			string RightSideString = flt_to_P(RightSide, p, c);
+
+			return LeftSideString + (RightSideString == String.Empty ? "" : ".") + RightSideString;
+		}
+
+		public static char int_to_Char(int d) {
+			if (d < 0 || d > 15)
+				throw new IndexOutOfRangeException();
+
+			string SymbolArray = "0123456789ABCDEF";
+			return SymbolArray.ElementAt(d);
+		}
+
+		public static string int_to_P(long n, long p) {
+			if (p < 2 || p > 16)
+				throw new IndexOutOfRangeException();
+			if (n == 0)
+				return "0";
+			bool HaveMinus = false;
+			if (n < 0) {
+				HaveMinus = true;
+				n *= -1;
+			}
+			string PNumber = string.Empty;
+
+			while (n > 0) {
+				PNumber += int_to_Char((int)(n % p));
+				n /= p;
+			}
+
+			if (HaveMinus)
+				PNumber += "-";
+
+			char[] TempArray = PNumber.ToCharArray();
+			Array.Reverse(TempArray);
+			return new string(TempArray);
+		}
+
+		public static string flt_to_P(double n, int p, int c) {
+			if (p < 2 || p > 16)
+				throw new IndexOutOfRangeException();
+			if (c < 0 || c > 10)
+				throw new IndexOutOfRangeException();
+
+			string PNumber = string.Empty;
+			for (int i = 0; i < c; ++i) {
+				PNumber += int_to_Char((int)(n * p));
+				n = n * p - (int)(n * p);
+			}
+			return PNumber;
+		}
+	}
+}
